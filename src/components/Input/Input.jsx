37c5951d1765ecc56aec54b668e0940a8e041447
Allan Verde <allangreen2906@gmail.com>
2021-11-25 11:59:23 -0300

    refactor(:heavy_minus_sign: remove component): remove component Input

--- src/components/Input/Input.jsx
+++
@@ -1,9 +0,0 @@
-import TextField from '@mui/material/TextField'
-
-function Input( { name } ) {
-    return (
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-    )
-}
-
-export default Input
